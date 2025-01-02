5ace90bc1e969516fc4d5429df1aac47b2820293
Novolin <captainnovolin@gmail.com>
2025-01-02 15:00:35 -0500

    fart

--- engine.py
+++ engine.py
@@ -6,17 +6,74 @@
 VERB_TYPES = { # List of valid global verbs, and their synonyms.
     "move":["move", "go", "walk", "run", "enter"],
     "use":["use", "try", "insert", "open", "unlock"],
-    "take":["take", "get", "grab", "pickup", "steal"],
+    "take":["take", "get", "grab", "pickup", "steal", "pick"],
     "look":["look", "examine", "see", "inspect", "observe", "peek", "check"]
 }
+GLOBAL_VERB_LIST = [] # list of verbs that will always be available
+for word in VERB_TYPES:
+    GLOBAL_VERB_LIST.extend(VERB_TYPES[word])
 # List of words the parser should ignore:
 IGNORED_WORDS = ["on", "the", "to"]
 
+class GameItem:
+    def __init__(self, item_name:str, item_alt_names:list = [], target = None, grabbable = False):
+        self.name = item_name
+        self.alt_nouns = item_alt_names
+        self.target = target
+        self.grabbable = grabbable
+        self.look_event = None
+
+    def set_item_target(self, newTarget):
+        self.target = newTarget
+
+
+
+
+class GameRoom: # Parent class for rooms in the game
+    def __init__(self, room_name:str):
+        self.name = room_name
+        self.neighbors = {
+            "N":{"open":None,"locked":None,"hidden":None}, 
+            "S":{"open":None,"locked":None,"hidden":None}, 
+            "E":{"open":None,"locked":None,"hidden":None}, 
+            "W":{"open":None,"locked":None,"hidden":None}, 
+            "U":{"open":None,"locked":None,"hidden":None}, 
+            "D":{"open":None,"locked":None,"hidden":None}, 
+            }
+        self.items = []
+        self.enter_event = None
+        self.look_event = None
+
+    def add_neighbor(self, neighbor, direction, state = "open"):
+        # adds a neighbor room to the list of rooms
+        if not self.neighbors[direction][state]:
+            self.neighbors[direction][state] = neighbor
+            return
+        print("{direction} neighbor slot in {neighbor} already filled.")
+        raise ValueError
+
+    def get_nouns(self):
+        noun_list = {}
+        for i in self.items:
+            noun_list[i.name] = i
+            if i.alt_nouns: # add refs for any alternate noun names we may have
+                for n in i.alt_nouns:
+                    noun_list[n] = i
+        for n in self.neighbors:
+            noun_list[n] = self.neighbors[n]["open"]
 class GameParser:
     # object to handle string decoding
     def __init__(self, start_room = 0, start_inventory = []):
         self.current_room = start_room # What room are we starting the game in
         self.current_items = start_inventory # Items we have in our starting inventory
+        
+
+    def get_allowed_nouns(self):
+        # returns a list of allowed words
+        allowed_words = []
+        allowed_words.extend(self.current_room.get_nouns())
+        for i in self.current_items:
+            allowed_words.append(i.get_nouns())
         
     def parse_incoming_string(self, string_to_parse): 
         # Returns a tuple of (verb, object 1, object 2)
@@ -24,16 +81,5 @@ class GameParser:
         # Object 2 is the target of the action, and may be None.
         # Examples: ("use", "key", "door"), ("move", "player", "north"), ("look", "room", None)
         output = [None, None, None]
-        listed_words = string_to_parse.split() # Split the string into separate words
-        check_word = 0
-        while check_word < len(listed_words): # roughing it out with a while loop
-            if listed_words[check_word] in IGNORED_WORDS: # we don't need that
-                check_word += 1
-                continue 
-            if listed_words[check_word] in VERB_TYPES["look"]: # We are looking at an object
-                if output[0] == None:
-                    output[0] = "look"
-                check_word += 1
-                continue
-            if listed_words[check_word] in VERB_TYPES["take"]: # We are taking an object
-                
+        nouns = self.get_allowed_nouns()
+        word_list = string_to_parse.split()
